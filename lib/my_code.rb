cd9d27994b2b0f033f220d756302d48e10833600
Lauren Birts <birtslauren@gmail.com>
2020-03-25 03:03:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,7 +9,7 @@ def map(source_array)
   new_array
 end
 
-def reduce(source_array, starting_value=nil) 
+def reduce(source_array, starting_value=0) 
   value = starting_value
   index = 0
   while index < source_array.length do 
