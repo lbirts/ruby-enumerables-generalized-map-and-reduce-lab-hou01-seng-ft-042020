f00e68bdf38ec679be20f215b42d6d38b8d3b2de
Lauren Birts <birtslauren@gmail.com>
2020-03-25 03:02:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,8 +3,8 @@ def map(source_array)
   new_array = []
   index = 0 
   while index < source_array.length do
-    new_array << yeild(source_array[index])
+    new_array << yield(source_array[index])
     index += 1 
   end 
   new_array
-end 
+end
